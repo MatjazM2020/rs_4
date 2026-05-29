d923f938b5df7dd439d47f4cc8e9ff1baabd3730
jimocool2 <muzan.jure@gmail.com>
2026-05-29 16:29:50 +0200

    added run slurm and stats

---
+++ mat_transpose/run_slurm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+GEM5_WORKSPACE=/d/hpc/projects/FRI/GEM5/gem5_workspace
+GEM5_ROOT=$GEM5_WORKSPACE/gem5
+GEM5_PATH=$GEM5_ROOT/build/VEGA_X86
+APPTAINER_IMG=$GEM5_WORKSPACE/gcn-gpu_v24-0.sif
+
+for CU in 2 4 8; do
+    srun --ntasks=1 --time=02:00:00 --output=log_cu${CU}.txt \
+        apptainer exec $APPTAINER_IMG \
+        $GEM5_PATH/gem5.opt --outdir=results/cu_${CU} \
+        $GEM5_ROOT/configs/example/apu_se.py \
+        --cpu-type=X86TimingSimpleCPU \
+        --num-cpus=1 \
+        --mem-size=8GB \
+        --mem-type=HBM_1000_4H_1x64 \
+        --num-compute-units=$CU \
+        -n 3 \
+        --benchmark-root=/d/hpc/home/jm1540/RS/rs_4/mat_transpose/bin/ \
+        --cmd=mat_transpose.bin &
+done
+
+wait
+echo "All done"
